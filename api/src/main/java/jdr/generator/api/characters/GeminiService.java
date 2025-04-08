697676bf0ea24097405d38100e97e27259a39ab1
NeoRyu <coupez.frederic@gmail.com>
2025-04-08 13:29:18 +0200

    [OK] JDR-Generator
    - UX WEB [port 5173]
    - API MIDDLEWARE [port 8080]
    - DB MYSQL [port 3306]
    - API GEMINI [port 3000]
    
    Signed-off-by: NeoRyu <coupez.frederic@gmail.com>

--- api/src/main/java/jdr/generator/api/characters/GeminiService.java
+++ api/src/main/java/jdr/generator/api/characters/GeminiService.java
@@ -18,7 +18,7 @@ import jdr.generator.api.characters.stats.CharacterJsonDataEntity;
 import jdr.generator.api.characters.stats.CharacterJsonDataModel;
 import jdr.generator.api.characters.stats.CharacterJsonDataService;
 import jdr.generator.api.config.IGeminiGenerationConfig;
-import jdr.generator.api.scala.HelloWorld;
+import jdr.generator.api.scala.ScalaMessage;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -84,8 +84,6 @@ public class GeminiService implements IGeminiGenerationConfig {
     @Override
     @Transactional
     public CharacterDetailsModel generate(DefaultContextJson data) {
-        HelloWorld.main(new String[]{});
-
         final String apiUrl = hostApiIA + "generate";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -164,6 +162,8 @@ public class GeminiService implements IGeminiGenerationConfig {
             }
 
             // FINALLY WE RETURN CHARACTER DETAILS
+            ScalaMessage.main(new String[]{});
+
             return characterDetailsModel;
         } catch (JsonProcessingException e) {
             LOGGER.error("Error parsing JSON response: {}", e.getMessage());
