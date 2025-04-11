3bc5b809e1e5ba5ce8c0d85ef47a986070eb82c5
NeoRyu <coupez.frederic@gmail.com>
2025-04-11 11:23:49 +0200

    [TESTS] JDR-Generator : Apprentissage développement SCALA
    - UX WEB [port 5173]
    - API MIDDLEWARE [port 8080]
    - DB MYSQL [port 3306]
    - API GEMINI [port 3000]
    
    Signed-off-by: NeoRyu <coupez.frederic@gmail.com>

--- api/src/main/scala/jdr/generator/api/scala/tests/Car.scala
+++ api/src/main/scala/jdr/generator/api/scala/tests/Car.scala
@@ -16,6 +16,7 @@ trait osef {
 trait AvgSpeed {
   var name: String
   def calcAvgSpeed(distKm: Double, timeHours: Double): Unit = {
+    println("----------------------------------------------------------------------------")
     println(s"Nous avons une distance de $distKm kilomètres à parcourir en $timeHours heures")
     val avgSpeed = distKm / timeHours
     println(s"Un véhicule $name devra donc rouler à une vitesse moyenne de $avgSpeed km/h")
@@ -47,22 +48,38 @@ class Car(test: String = "undefined") extends Vehicle(test) with osef {
     println(curry(2)(8))// on affiche le résultat d'une fonction curry multipliant les deux entiers en appelant le champs multiply : 2*8=16
   }
 
-  def calcTotalCost(nbCar: Int, cost: Double = 25000.0): Unit = {
-    val subTotal = (nb: Int) => {
-      cost * nb
-    }
+  def calcTotalCost(nbCar: Int, model: String): Unit = {
+    val taxe = 1.2; // TVA de 20%
+    val taxePercent: String = "(TVA " + (taxe * 100 - 100) + "% incl.)"
+    val cost = Map(
+      "Citroen C6" -> 50450.51,
+      "Citroen C3" -> 15750.43,
+      "Citroën Traction 15 Six Cabriolet" -> 612440.0
+    ) // database de model/prix
+    val subTotal = (qte: Int) => {
+      qte * cost(model)
+    } // calcul du sous-total quantité x prix du model
     val calcTva = (sub: Double) => {
-      val tva = 1.2 // 20%
-      sub * tva
-    }
+      sub * taxe
+    } // calcul sous total avec tva appliquée
 
-    // FONCTIONS COMPOSEES :
-    val total1 = (calcTva compose subTotal)(nbCar)
+    // TEST DE FONCTIONS COMPOSEES :
+    def total1(qteCar: Int = nbCar) = (calcTva compose subTotal)(qteCar)
     // compose appel d'abord la dernière fonction subTotal (et utiliser nbCar) avant d'appeler la premiere (et utiliser subTotal comme param)
     val total2 = (subTotal andThen calcTva)(nbCar)
     //andThen va d'abord appeler la premiere methode subTotal (et utiliser nbCar) avant d'appeler la suivante (et utiliser subTotal comme param)
+    if (total1(nbCar) == total2) {
+      println(s"Prix d'achat $taxePercent de $nbCar x $model : $total2 €")
+      println(s"> [INFO] Prix unitaire : "+ cost(model) + s" € / Prix unitaire $taxePercent : " + total1(1) + " €")
+    } else { // Ne devrait pas arriver dans notre cas présent,
+      val getHighestPrice: Double = scala.collection.mutable.ListBuffer(total1(nbCar), total2).max
+      // Ici les prix devant être normalement identique, on peut éviter une erreur préjudiciable au vendeur en prenant le prix le plus élevé
+      val getBestReductionPrice: Double = scala.collection.mutable.ListBuffer(total1(nbCar), total2).min
+      // ou au contraire utiliser min qui aurait permis de détecter par exemple la meilleure réduction si une telle fonctionnalité avait été mise en place
+      println(s"[INFO] Exemple de prix d'achat $taxePercent de $nbCar x $model : $getHighestPrice € (avec toutes les options)")
+      println(s"[INFO] Exemple de prix d'achat $taxePercent de $nbCar x $model : $getBestReductionPrice € (sans option ajoutée)")
+    }
 
-    println(s"Prix pour l'achat de $nbCar : $total1 || $total2 (les deux montant doivent être identique)")
   }
 }
 
@@ -73,6 +90,7 @@ class Car(test: String = "undefined") extends Vehicle(test) with osef {
 // de paramètres et sont en fait implémentés en tant qu'instance d'une classe synthétique.
 object Car {
   private val voiture = new Car("Citroen C6")
+  // voiture un objet singleton instanciant la classe "Car".
   private val voitureConso = new CarConso(72,10,8.5,11.2,10.0)
   private val voitureDist = new CarDist(72d,10d,8.5,11.2,10.0)
 
@@ -81,17 +99,16 @@ object Car {
 
     new Vehicle calcAvgSpeed (100, 2) // inconnu
     new Car calcAvgSpeed (100, 2) // sans marque
+
     // On instancie un nouvel objet Car, qui est une instance de la classe Car dans le corps de l'objet compagnon
     // Cet objet est en fait une instance de la classe compagnon Car, et non de l'objet compagnon.
     voiture calcAvgSpeed (200, 2) // Citroen C6
-    voiture.vroom()
-    voiture.calcTotalCost(2) // on veux acheter deux voitures
-    // voiture est aussi un objet singleton instanciant la classe "Car".
-
+    // voiture.vroom()
     voitureConso kmParPlein "Mixte"
     // On instancie d'autres classes dans cet objet compagnon "Car" ...
     voitureDist kmParPlein "Route"
     voitureDist kmParPlein "urbain"
     // ... CarConso et CarDist, permettant de définir plus concrètement l'objet en question
+    voiture.calcTotalCost(10 ,"Citroen C6") // on veux acheter deux voitures
   }
 }
--- api/src/main/scala/jdr/generator/api/scala/tests/ScalaTests.scala
+++ api/src/main/scala/jdr/generator/api/scala/tests/ScalaTests.scala
@@ -1,7 +1,9 @@
 package jdr.generator.api.scala.tests
 
+import jdr.generator.api.scala.tools.TypescriptOperators._
 import jdr.generator.api.scala.tools._
 
+import java.awt.Color
 import java.util.{Calendar, Date}
 
 // Généricité : classe conteneur Reference, pouvant être vide ou pointer vers un objet typé
@@ -24,14 +26,23 @@ object ScalaTests {
   case class Personne(name : String,addr: String, age: String)
 
   // COLLECTIONS
-  var l = List(1,2,3,4,5)
-  val s = Set(3,6,9,12,15)
-  val m = Map("a" -> 1, "b" -> 2, "c" -> 3, "d" -> 4, "e" -> 5)
   var planetes = List(("Mercure", 57.9), ("Venus", 108.2), ("Mars", 227.9), ("Jupiter", 778.3))
+  var cl = List(1,2,3,4,5)
+  var clb = scala.collection.mutable.ListBuffer("x", "y", "z")
+  var cm = scala.collection.mutable.Map("x" -> 24, "y" -> 25, "z" -> 26)
+  var cs1 = Set(Color.red, Color.green, Color.blue)
+  val cseq = Seq(7,4,1,8,5,2,9,6,3,0,10) // on ne peut que update la place de 2 items, pas en ajouter
+  val cseq1 = Seq("Avion","Voiture","Bateau")
+  val cseq2 = Seq("Maison","Bateau","Aeroport","Quai") // On met deux fois Bateau
+  var iterator = Iterator("x", "y", "z")
+  var css = scala.collection.mutable.SortedSet("hello", "world")
+  var cb = scala.collection.mutable.Buffer("x", "y", "z")
+  var cis = IndexedSeq(1.0, 2.0)
   /*
-  List = mutable
-  Set = immutable
-  Map = immutable
+    Par défaut, Scala sélectionne toujours des collections immuables. Par exemple, Set sans préfixe ou sans import sera un ensemble immuable.
+    Pour obtenir les versions mutables par défaut, on doit écrire explicitement collection.mutable.Set, ou collection.mutable.Iterable.
+    - IMMUTABLE : https://docs.scala-lang.org/resources/images/tour/collections-immutable-diagram.svg
+    - MUTABLE   : https://docs.scala-lang.org/resources/images/tour/collections-mutable-diagram.svg
    */
 
   // Ajout d'une fonction idiomatique de suppression d'une planète dans une List
@@ -55,24 +66,53 @@ object ScalaTests {
     ScalaIntSetTests()
 
     // 5/ Jeu d'essai sur des collections
-    println("5.1-1/ List : " + l.head)
-    l = 10 :: l
-    println("5.1-2/ List : " + l.head + " || " + l(0)) // List(1,2,3,4,5)
-    println("5.2/ Set: " + s(3)) // Set(3,6,9,12,15)
-    println("5.3/ Map: " + m("b")) // Map("a" -> 1, "b" -> 2, "c" -> 3, "d" -> 4, "e" -> 5)
-    planetes = ("Terre", 149.6) :: planetes
+    println("5.1-1/ List : " + cl.head + " = " + cl(0))
+    cl = 10 :: cl // on ajoute un objet Int 10 dans le head de la list
+    println("5.1-2/ List : " + cl.head + " > " + cl(1) + " > " + cl(2))
+
+    clb += "a" // on peux ajouter des String avec += dans ListBuffer
+    clb -= "y" // on peux supprimer des String avec -= dans ListBuffer
+    println("5.2-1/ ListBuffer : " + clb )
+    println("5.2-2/ ListBuffer (min/max): " + clb.min + " >...> " + clb.max  )
+
+    println("5.3-1/ Set: " + cs1.head + cs1.tail) // head affiche le premier element, tail tout sauf le premier
+    val cs2 = Set(Color.red, Color.black, Color.white) // red est un doublon
+    var csSentence = cs1 ++ cs2 // red n'apparaitra qu'une fois dans sentence en combinant les deux Set
+    println("5.3-2/ Set (cs1+cs2): " + csSentence) // ce qui est le cas, on remarque que les valeur sont aussi mélangées
+
+    println("5.4-1/ Map: x = " + cm("x")) // On peux accéder a une value d'un Map par sa key, ici x = 24
+    cm.keys.foreach( i => println(s"key: $i | value: " + cm(i)) ) // parcours du map
+
+    println("5.4-2.a/ Seq: " + cseq)
+    println("5.4-2.b/ Seq (sorted): " + cseq.sorted) // tri sur des Int
+    println("5.4-2.c/ Seq1 : " + cseq1)
+    println("5.4-2.c/ Seq2 : " + cseq2)
+    val concatSeq = cseq1++cseq2 // ++ permet la concatenation de deux listes
+    println("5.4-2.d/ concatSeq : " + concatSeq) // la concat les met dans le meme ordre que ceux de l'ajout
+    println("5.4-2.d/ concatSeq (sorted) : " + concatSeq.sorted)  // Tri sur des String
+
+    planetes = ("Terre", 149.6) :: planetes // On ajoute une entrée dans le head de la List ; si c'était un val et non un var cette commande serait impossible
+    println("5.5-0/ planètes : " + planetes) // On vérifie notre liste de planetes
     planetes.foreach {
       case ("Terre", distance) =>
         println(s"5.5-1/ Notre planète est à une distance de $distance millions de km du Soleil")
       case _ =>
     }
-    planetes = remove(planetes.head, planetes)
+    planetes = remove(planetes.head, planetes) // on appel une fonction idiomatique définie préalablement pour suprrimer Terre qui se trouve en head de List
+    println("5.5-2/ planètes : " + planetes) // On vérifie notre liste de planetes
     planetes.foreach {
-      case ("Terre", distance) => println(s"5.5-2/ Ce message ne dois pas apparaitre")
+      case ("Terre", distance) => println(s"5.5-3/ Ce message ne dois donc jamais apparaitre")
       case _ =>
     }
 
-
+    println("5.6/ Iterator : " + iterator)
+    while (iterator.hasNext) { // On va parcourir l'itération
+      print(iterator.next + (iterator.hasNext ? " / " :: " [END]")) // affichant un a un les item et ajoutant un / si il y en a un apres, sinon on affiche [END]
+      // (value condition ? true :: false)
+      // Il s'agit de ma librairie TypescriptOperators permettant de simuler le comportement de opérateur conditionnel ternaire reproduisant ce qui ce fait en Typescript
+      // Cela équivaut a :  if(value condition) { true } else { false } que je trouve moins ergonomique
+      // (iterator.hasNext ?? " / ") n'est ici pas possible car on obtiendrai "xtrueytruezfalse", true etant la valeur non empty de hasNext elle serai ajoutée
+    }
 
   }
 
