739f80992f9741eada0a924e21f0f0d5ae19b04d
NeoRyu <coupez.frederic@gmail.com>
2025-05-13 10:31:01 +0200

    [GITHUB ACTIONS] JDR-Generator : Configuration du workflow de linting, formatting et analyse statique -> [WIP] Gros refactoring du code source pour s'appuyer sur le google_checks.xml pour corriger les erreurs SpotBugs (problèmes de code), ainsi que correction des erreurs OWASP (vulnérabilités des dépendances) --> voir pom.xml (j'ai skip les tests pour repermettre un build, quelques milliers d'erreurs corrigé mais besoin d'investiguer un peu plus pour le reste)
    - mysql-container: 3306
    - api-container: 8080
    - web-container: 80
    - gemini-container: 3001
    - openai-container: 3002
    
    Signed-off-by: NeoRyu <coupez.frederic@gmail.com>

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,12 +1,42 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
 import {defineConfig} from "eslint/config";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
 import js from "@eslint/js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig([
+  // Règles JavaScript et TypeScript de base
   {
-    files: ["**/*.js"],
+    files: ["**/*.{js,ts,jsx}"],
     extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.browser },
+    },
   },
+
+  // Règles TypeScript
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: { "@typescript-eslint": tseslint.plugin },
+    extends: [
+      "@typescript-eslint/recommended-type-checked",
+      "@typescript-eslint/stylistic-type-checked",
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: path.resolve(__dirname, "tsconfig.json"),
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
+
 ]);
-
-
