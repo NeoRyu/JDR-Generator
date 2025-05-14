bc4ad8deb14bae5a989567c97b41e4ac0fd98411
NeoRyu <coupez.frederic@gmail.com>
2025-05-14 10:03:06 +0200

    [GITHUB ACTIONS] JDR-Generator : Configuration du workflow de linting, formatting et analyse statique - FIX clean/build/start all modules OK
    - mysql-container: 3306
    - api-container: 8080
    - web-container: 80
    - gemini-container: 3001
    - openai-container: 3002
    
    Signed-off-by: NeoRyu <coupez.frederic@gmail.com>

--- openai/eslint.config.js
+++ openai/eslint.config.js
@@ -1,18 +1,14 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import {defineConfig} from "eslint/config";
-import path from "node:path";
-import {fileURLToPath} from "node:url";
 import js from "@eslint/js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
 
-export default defineConfig([
   // Règles JavaScript et TypeScript de base
   {
     files: ["**/*.{js,ts}"],
-    extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -29,28 +25,9 @@ export default defineConfig([
       "@typescript-eslint/stylistic-type-checked",
     ],
     languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: [
-          path.resolve(__dirname, "tsconfig.json"),
-        ],
-        tsconfigRootDir: __dirname,
-        sourceType: "module",
-        ecmaVersion: "latest",
-      },
+      parser: '@typescript-eslint/parser',
     },
     rules: {
-      // TODO : Commenter les règles plus tard pour corriger les errors manuellement
-      "@typescript-eslint/prefer-optional-chain": "off",
-      "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/await-thenable": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
     },
   },
-]);
+];
