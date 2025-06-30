d84c24e54350c9bc4e144d3a7630201d01b467aa
NeoRyu <coupez.frederic@gmail.com>
2025-06-30 13:24:43 +0200

    [HELM] JDR-Generator : WIP - Déploiement Google Cloud depuis Kubernetes HELM - WIP : (test) pointage des requetes vers le bon host > TODO : github actions deploy-kubernetes-helm.yml (required cluster gcloud)
    - FIXED cpu/memory in values.yaml to deploy cluster in Google Cloud !
    
    FIX : cd freepik && npm run format
    FIX : code-quality.yml : PMD :: adding nvd nist api key
    - https://nvd.nist.gov/developers/api-key-requested
    - .m2/settings.xml ( mvn help:effective-settings )
    - NVD_API_KEY in github /settings/secrets/actions
    - mvn org.owasp:dependency-check-maven:12.1.0:update-only -Ddata.purge=true
    FIX : code-quality.yml : POM.xml = checkstyle GOOGLE :: mvn spotless:apply to fix files
    TODO : spotbugs (skipped : code refactoring required)
    
    ______
    
    Rappel des ports de l'application
    - mysql-container: 3306
    - api-container: 8080
    - web-container: 80
    - gemini-container: 3001
    - openai-container: 3002
    -> http://localhost:5173
    - kubernetes : 3080
    -> http://localhost:3080
    - jenkins-container : 9000
    -> http://localhost:9000
    
    Signed-off-by: NeoRyu <coupez.frederic@gmail.com>

--- web/src/services/getListCharactersFull.service.ts
+++ web/src/services/getListCharactersFull.service.ts
@@ -7,7 +7,7 @@ export interface CharactersResponse {
   data: CharacterFull[];
 }
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"; // Fallback .env.local
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://34.155.16.213:80"; // Fallback local : "http://localhost:8080"
 
 export const getListCharactersFull = () => {
   return useQuery<AxiosResponse<CharactersResponse>, Error>(
